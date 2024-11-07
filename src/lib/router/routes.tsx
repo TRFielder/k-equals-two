429f1d6ec5b296dc6c5bf1976145ee7c34b5904c
Tom Fielder <me@tomfielder.co.uk>
2024-11-07 00:31:26 +0000

    Set husky to use its own lint script

--- src/lib/router/routes.tsx
+++ src/lib/router/routes.tsx
@@ -1,4 +1,4 @@
 import type { RouteObject } from "react-router-dom";
 import Welcome from "@/features/welcome";
-console.log("Hi");
+
 export const routes: RouteObject[] = [{ path: "/", element: <Welcome /> }];
