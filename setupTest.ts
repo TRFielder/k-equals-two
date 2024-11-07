c9491770241ab56c6463ed4807406bdaaa396030
Tom Fielder <me@tomfielder.co.uk>
2024-11-07 23:09:36 +0000

    Correct matcher import

--- setupTest.ts
+++ setupTest.ts
@@ -1,6 +1,6 @@
 import { afterEach, expect } from "vitest"
 import { cleanup } from "@testing-library/react"
-import matchers from "@testing-library/jest-dom/matchers"
+import * as matchers from "@testing-library/jest-dom/matchers"
 
 expect.extend(matchers)
 // runs a clean after each test case (e.g. clearing jsdom)
