9a0053cf53dfb897a33c84605f5bf34038835293
Tom Fielder <me@tomfielder.co.uk>
2024-11-07 00:30:35 +0000

    Add husky. Switch eslint 9 -> biome

--- src/lib/router/routes.tsx
+++ src/lib/router/routes.tsx
@@ -1,4 +1,4 @@
-import { RouteObject } from "react-router-dom";
+import type { RouteObject } from "react-router-dom";
 import Welcome from "@/features/welcome";
-
+console.log("Hi");
 export const routes: RouteObject[] = [{ path: "/", element: <Welcome /> }];
